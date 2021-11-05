f63b890ae78871505be8af71732cd00db6244674
Grace <grace.e.osullivan@students.ittralee.ie>
2021-11-05 10:57:52 +0000

    Test commit to branch

---
+++ src/DecoratorPattern.java
@@ -0,0 +1,5 @@
+public class DecoratorPattern {
+    public static void main(String[] args) {
+        System.out.println("Testing commit to branch");
+    }
+}
--- src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Testing commit to repository");
-        System.out.println("Update Test");
-        System.out.println("Update Test 2");
-    }
-}
