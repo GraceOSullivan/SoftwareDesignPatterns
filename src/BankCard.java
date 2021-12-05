ca3e189f3f8261e3727fc6bf87de5a1551cf9346
Grace <grace.e.osullivan@students.ittralee.ie>
2021-12-05 11:16:16 +0000

    Applying singleton pattern - constructor mde private and static getInstance() method created

--- src/BankCard.java
+++ src/BankCard.java
@@ -1,6 +1,7 @@
 import java.util.Date;
 
 public class BankCard {
+    private static BankCard bankCard;
     private BankAccount account;
     private CardType cardType;
     private int cardNumber;
@@ -13,5 +14,13 @@ public class BankCard {
         this.cardNumber = cardNumber;
         this.expirationDate = expirationDate;
         this.csv = csv;
+    }
+
+
+    public static BankCard getInstance(BankAccount account, CardType cardType, int cardNumber, Date expirationDate, int csv){
+        if(bankCard == null){
+            bankCard = new BankCard(account, cardType, cardNumber, expirationDate, csv);
+        }
+        return bankCard;
     }
 }
