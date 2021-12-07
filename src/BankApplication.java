bf74f55b79d54ddb1f90fc1605ba5c3f26b02bc1
Grace <grace.e.osullivan@students.ittralee.ie>
2021-12-07 13:30:10 +0000

    Extracted variables in BankApplication driver to allow for easier debugging

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -1,15 +1,24 @@
+import javax.smartcardio.Card;
+
 public class BankApplication {
     public static void main(String[] args) {
 
         Person grace = new Person("Grace", "Killarney");
+        int accountNum = 1234;
+        double balance = 1000.00;
+        BankCard debit = BankCard.getInstance(CardType.DEBIT);
+        BankCard credit = BankCard.getInstance(CardType.CREDIT);
 
         //Current, Student, Online Account
-        BankAccount currentStudentOnlineAccount = new Online(new Student(new CurrentAccount(grace, 1234, 10000.00, BankCard.getInstance(CardType.DEBIT))));
+        BankAccount currentStudentOnlineAccount = new Online(new Student(new CurrentAccount(grace, accountNum, balance, debit)));
 
         //Savings, Personal, Graduate Account
-        BankAccount savingsPersonalGraduateAccount = new Graduate(new Personal(new SavingsAccount(grace, 1234, 1000.00, BankCard.getInstance(CardType.CREDIT))));
+        BankAccount savingsPersonalGraduateAccount = new Graduate(new Personal(new SavingsAccount(grace, accountNum, balance, credit)));
 
-        System.out.println(currentStudentOnlineAccount.describeAccount());
-        System.out.println(savingsPersonalGraduateAccount.describeAccount());
+        //System.out.println(currentStudentOnlineAccount.describeAccount());
+        //System.out.println(savingsPersonalGraduateAccount.describeAccount());
+
+        System.out.println(savingsPersonalGraduateAccount.getBalance());
+
     }
 }
