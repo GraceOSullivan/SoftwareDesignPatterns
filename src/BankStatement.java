ea1efee3dfee110dedbb646f6bed7c0cc7a3d92c
Grace <grace.e.osullivan@students.ittralee.ie>
2021-12-09 12:27:05 +0000

    StatementTypes added - violating OCP and DRY

--- src/BankStatement.java
+++ src/BankStatement.java
@@ -1,13 +1,32 @@
 import java.util.ArrayList;
 
 public class BankStatement {
+    private StatementType statementType;
+    private static BankStatement statement;
+
+    BankStatement(StatementType statementType){
+        this.statementType = statementType;
+    }
+
     private void printBankStatement(BankAccount account){
         ArrayList<String> allTransactions = account.getTransactions();
-        System.out.println("Bank Statement for " + account.getAccNum() +
-                "\n -------------------------------------");
-        for (String transaction : allTransactions) {
-            System.out.println(transaction);
+        if(statement.statementType.equals(StatementType.ONLINE)){
+            System.out.println("SENDING ONLINE BANK STATEMENT");
+            System.out.println("Bank Statement for " + account.getAccNum() +
+                    "\n -------------------------------------");
+            for (String transaction : allTransactions) {
+                System.out.println(transaction);
+            }
+            System.out.println("Balance: €" + account.getBalance());
         }
-        System.out.println("Balance: €" + account.getBalance());
+        else if(statement.statementType.equals(StatementType.POSTAL)){
+            System.out.println("POSTING BANK STATEMENT");
+            System.out.println("Bank Statement for " + account.getAccNum() +
+                    "\n -------------------------------------");
+            for (String transaction : allTransactions) {
+                System.out.println(transaction);
+            }
+            System.out.println("Balance: €" + account.getBalance());
+        }
     }
 }
---
+++ src/StatementType.java
@@ -0,0 +1,4 @@
+public enum StatementType {
+    ONLINE,
+    POSTAL
+}
